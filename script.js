4f6e84ad5cefb70dfd24220c5a51dc803cef9086
Reskipanginja <reskipanginja04@gmail.com>
2024-10-24 11:23:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+</script>
+</head>
+<body>
+    <header>
+        <h1>PROFIL</h1>
+    </header>
+
+    <section class="profile">
+        <div class="profile-info">
+            <img src="WhatsApp Image 2024-10-24 at 10.54.28.jpeg" alt="Foto Profil" class="profile-photo">
+            <h2>RESKI PANGINJA</h2>
+            <p>.</p>
+            <h3>KONTAK</h3>
+            <p>Email: reskipanginja04@gmail.com</p>
+            <p>Telepon: 081244250234</p>
+            <h3>Alamat</h3>
+            <p>Jl.Kesenangan 7 no 326, blok E</p>
+            <h3>PENDIDIKAN</h3>
+            <p>SDN 159 Pakatan</p>
+            <p>SMP 35 Makassar</p>
+            <p>SMK Kartika XX-1 Makassar</p>
+            <p>Politeknik Negeri Ujung Pandang</p>
+            <h3>PENGALAMAN ORGANISASI</h3>
+            <p>Anggota pengkaderan HME-PNUP periode 22/23</p>
+            <p>KOORDINATOR MINAT DAN BAKAT HME-PNUP Periode 23/24</p>
+        </div>
+
+        <div class="social-media">
+            <h3>Ikuti Saya</h3>
+            <a href="#" onclick="loginToSocialMedia('facebook')"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" onclick="loginToSocialMedia('twitter')"><i class="fab fa-twitter"></i></a>
+            <a href="#" onclick="loginToSocialMedia('instagram')"><i class="fab fa-instagram"></i></a>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; MAKASSAR, 24 OKTOBER 2024.</p>
+    </footer>
+</body>
+</html>
